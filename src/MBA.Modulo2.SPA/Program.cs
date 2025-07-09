c8239d6e55abd348bb3d4935303649cf5761d6b1
Douglas costa <douglasalgustocosta@gmail.com>
2025-07-09 01:11:52 -0300

    alteração para acessar serviços de endpoint com a api de categorias

--- src/MBA.Modulo2.SPA/Program.cs
+++ src/MBA.Modulo2.SPA/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Authorization;
 using MBA.Modulo2.Spa.Services.Autentica;
+using MBA.Modulo2.Spa.Services.Api;
 
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -17,13 +18,17 @@ builder.Services.AddServices();
 builder.Services.AddHttpClient("ApiSpa", options =>
 {
     options.BaseAddress = new Uri("https://localhost:7015/");
-});
+}).AddHttpMessageHandler<CustomHttpHandler>();
+
+builder.Services.AddScoped<CustomHttpHandler>();
 
 builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddAuthorizationCore();
 
 builder.Services.AddScoped<AuthenticationStateProvider, ApiAuthenticationStateProvider>();
 builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<ICategoriaService, CategoriaService>();
+
 
 
 await builder.Build().RunAsync();
