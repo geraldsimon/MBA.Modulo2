892733e13be975cec44e85662174af0c6f2099eb
Silvio Kinaake <skinaake@hotmail.com>
2025-07-10 01:00:07 -0300

    Cria entidade cliente

---
+++ src/MBA.Modulo2.Business/Services/Interface/ICustomerService.cs
@@ -0,0 +1,9 @@
+﻿using MBA.Modulo2.Data.Domain;
+
+namespace MBA.Modulo2.Business.Services.Interface
+{
+    public interface ICustomerService
+    {
+        Task AddAsync(Cliente customer);
+    }
+}
---
+++ src/MBA.Modulo2.Data/Repository/Implamentation/ClienteRepository.cs
@@ -0,0 +1,12 @@
+﻿using MBA.Modulo2.Data.Domain;
+using MBA.Modulo2.Data.Repository.Interface;
+
+namespace MBA.Modulo2.Data.Repository.Implamentation
+{
+    public class ClienteRepository : Repository<Cliente>, IClienteRepository
+    {
+        public ClienteRepository(AppDbContext context) : base(context)
+        {
+        }
+    }
+}
