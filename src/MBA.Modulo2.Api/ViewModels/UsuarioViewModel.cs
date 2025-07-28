9f0199dc789a8c9d264f08acb9c55b0549afb367
Leonardo <leosilvarocha.lsr@gmail.com>
2025-07-28 16:40:27 -0300

    ajuste cadastro do cliente

--- src/MBA.Modulo2.Api/ViewModels/UsuarioViewModel.cs
+++ src/MBA.Modulo2.Api/ViewModels/UsuarioViewModel.cs
@@ -4,6 +4,10 @@ namespace MBA.Modulo2.Api.ViewModels;
 
 public class RegisterUserViewModel
 {
+
+    [Required(ErrorMessage = "Nome é obrigatório.")]
+    public string Nome { get; set; }
+
     [Required(ErrorMessage = "O campo {0} é obrigatório")]
     [EmailAddress(ErrorMessage = "O campo {0} está em formato inválido")]
     public string Email { get; set; }
