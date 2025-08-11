373efe65321681a8b3dce31db7663d348e38ad54
Silvio Kinaake <skinaake@hotmail.com>
2025-08-10 22:58:20 -0300

    cria-pagina-editar-cliente-spa

--- src/MBA.Modulo2.SPA/ExternalApi/ClienteHttpClient.cs
+++ src/MBA.Modulo2.SPA/ExternalApi/ClienteHttpClient.cs
@@ -1,5 +1,7 @@
 ﻿using MBA.Modulo2.Spa.ViewModels;
+using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Text;
 using System.Text.Json;
 
 namespace MBA.Modulo2.Spa.ExternalApi
@@ -45,5 +47,32 @@ namespace MBA.Modulo2.Spa.ExternalApi
             return products ?? new ProdutoDetalhesViewModel();
         }
 
+        public async Task<ClienteViewModel> ObterPorIdAsync(Guid id)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, $"{_api}/{id}?api-version=1.0");
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var response = await _httpClient.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+
+            var json = await response.Content.ReadAsStringAsync();
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+            var cliente = JsonSerializer.Deserialize<ClienteViewModel>(json, options);
+
+            return cliente ?? new ClienteViewModel();
+        }
+
+        public async Task AtualizarClienteAsync(ClienteViewModel cliente)
+        {
+            var json = JsonSerializer.Serialize(cliente);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await _httpClient.PutAsync($"{_api}/{cliente.Id}?api-version=1.0", content);
+
+            response.EnsureSuccessStatusCode();
+        }
     }
 }
